46db9c5051d2f16d17457ec1ea6a0dab4239e04a
DevPrice <DevPrice@users.noreply.github.com>
2026-09-08 23:19:27 -0700

    register the gdextension entry point and MODULE_INITIALIZATION_LEVEL_SCENE hook

---
+++ src/register_types.h
@@ -0,0 +1,9 @@
+#ifndef GODOT_VERSE_REGISTER_TYPES_H
+#define GODOT_VERSE_REGISTER_TYPES_H
+
+#include <godot_cpp/core/class_db.hpp>
+
+void initialize_gdextension_types(godot::ModuleInitializationLevel p_level);
+void uninitialize_gdextension_types(godot::ModuleInitializationLevel p_level);
+
+#endif // GODOT_VERSE_REGISTER_TYPES_H
