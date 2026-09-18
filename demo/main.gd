56dbc54a3ad8298463fb91edc202ed72209ec080
DevPrice <DevPrice@users.noreply.github.com>
2026-09-18 02:39:39 -0700

    remove useless comments

--- demo/main.gd
+++ demo/main.gd
@@ -1,12 +1,6 @@
-extends Node2D
+class_name MainScript extends Node2D
 
 @export var mover: Mover
 
 func _ready() -> void:
-	print("Wow " + mover.Greeting)
-	for method: Dictionary in mover.get_method_list():
-		print(method.name)
-
-enum MyEnum {
-	X
-}
+	print("Greeting from GDScript: %s" % mover.Greeting)
